cac9e9cbf061e1e8cc6349f0973498c33a698ffd
SheeRMooN <legionerus@yandex.ru>
2021-02-19 18:55:47 +0300

    Initial commit

---
+++ src/main/java/Test/Info.java
@@ -0,0 +1,11 @@
+package Test;
+
+public interface Info {
+    static void sssf(){
+        System.out.println("dsada");
+    }
+
+    default void  ss(){
+        System.out.println("sdasda");
+    }
+}
---
+++ src/main/java/Test/Testt.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Testt implements Info {
+    public static void main(String[] args) {
+        Info.sssf();
+
+        Testt testt = new Testt();
+        testt.ss();
+
+    }
+}
